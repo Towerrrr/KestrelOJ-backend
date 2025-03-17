24644763fb6c0e8d2a25c29fb144f4e73e5bb674
Towerrrr <1611993104@qq.com>
2025-03-17 22:28:42 +0800

    初始模版

---
+++ src/main/java/com/t0r/kestreloj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.t0r.kestreloj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.t0r.kestreloj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
